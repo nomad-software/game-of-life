50b6d2ae722eb07744199d05b84d6cb822140052
Gary Willoughby <dev@nomad.uk.net>
2020-04-16 18:45:20 +0100

    Added a seeding benchmark.

--- game/game_test.go
+++ game/game_test.go
@@ -4,7 +4,19 @@ import (
 	"testing"
 )
 
-func BenchmarkGame(b *testing.B) {
+func BenchmarkSeeding(b *testing.B) {
+	game := NewGame(200, 60)
+
+	b.SetBytes(200 * 60 * 4)
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for x := 0; x < b.N; x++ {
+		game.Seed()
+	}
+}
+
+func BenchmarkIncubation(b *testing.B) {
 	game := NewGame(200, 60)
 
 	b.SetBytes(200 * 60 * 4)
